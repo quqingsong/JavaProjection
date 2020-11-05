1001cb69764ff40b8f176eaa9d31b13a6e0fedee
quqingsong <1021237024@qq.com>
2020-11-05 11:20:06 +0800

    这个是aop介于xml与注解版之间版本

--- aop_demo/src/main/java/com/dingler/Aspect/MyAspect.java
+++ aop_demo/src/main/java/com/dingler/Aspect/MyAspect.java
@@ -13,6 +13,14 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyAspect {
+    /**
+     * execution（）               表达式的主体；
+     * 第一个”*“符号             表示返回值的类型任意；
+     * com.sample.service.impl	   AOP所切的服务的包名，即，我们的业务部分
+     * 包名后面的“..”  	       表示当前包及子包
+     * 第二个“*”  	           表示类名，*即所有类。此处可以自定义，下文有举例
+     *   .*(..)	                   表示任何方法名，括号表示参数，两个点表示任何参数类型
+     */
     @Before("execution(* com.dingler.service.serviceImpl..*.*(..))")
     public void before(){
         System.out.println("=====================方法执行前=====================");
